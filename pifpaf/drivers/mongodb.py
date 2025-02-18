900656ec4cb1e0e0f5493dabc4400743c9834dd1
Anna “CyberTailor” <cyber@sysrq.in>
2025-02-18 15:53:19 +0500

    Capture startup line in MongoDB JSON logs
    This format is used since MongoDB 4.4.
    
    Example line:
    
    {"t":{"$date":"2025-02-18T15:42:36.070+05:00"},"s":"I",  "c":"NETWORK",  "id":23016,   "ctx":"listener","msg":"Waiting for connections","attr":{"port":29000,"ssl":"off"}}

--- pifpaf/drivers/mongodb.py
+++ pifpaf/drivers/mongodb.py
@@ -53,7 +53,7 @@ class MongoDBDriver(drivers.Driver):
              "--port", str(self.port),
              "--dbpath", self.tempdir,
              "--bind_ip", "localhost"] + storage_engine,
-            wait_for_line="waiting for connections on port %d" % self.port)
+            wait_for_line=r"[Ww]aiting for connections")
 
         self.putenv("MONGODB_PORT", str(self.port))
         self.putenv("URL", "mongodb://localhost:%d/test" % self.port)
